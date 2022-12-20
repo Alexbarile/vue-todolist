1e2cdaad619d5eaf7dea23dce1ffb4a5d9ab9f9e
“Alexbarile” <alexbarile93fg@yahoo.it>
2022-12-20 21:21:02 +0100

    esercizio completo più BONUS 1 e 2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,63 @@ const{
 createApp({
     data(){
         return{
-            mex: 'ciao'
+            newTask: '',
+            todolist:[
+                {
+                    text:'Andare a fare la spesa per la Vigilia di Natale e per Capodanno',
+                    done: false,
+                },
+                {
+                    text:'Comprare prosecco e spumante',
+                    done: false,
+                },
+                {
+                    text:'Comprare Pandoro e Panettone',
+                    done: true,
+                },
+                {
+                    text:'Comprare Cotechino e Lenticchie',
+                    done: true,
+                },
+                {
+                    text:'Fare shopping per outfit di Capodanno',
+                    done: true,
+                },
+                {
+                    text:'Decidere location per festa di Capodanno',
+                    done: false,
+                },
+                {
+                    text:'Fare gli auguri al Team Boolean',
+                    done: false,
+                },
+            ]
         }
+    },
+
+    methods:{
+        // Aggiungo elementi alla lista 
+        addTask(){
+            let object = {
+                text: this.newTask,
+                done: false,
+            }
+            this.todolist.push(object);
+            this.newTask = '';
+        },
+        // Rimuovo elementi dalla lista
+        removeTask(index){
+            this.todolist.splice(index, 1);
+        },
+        // Controllo se gli elementi nella lista hanno done = true.
+        checkTask(index){
+            let task = this.todolist[index];
+            if(task.done == true){
+                task.done = false;
+            }
+            else{
+                task.done = true;
+            }
+        },
     }
 }).mount('#app')
